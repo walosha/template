778d6a5260c242f26d7cdf12f942ed16e86ee860
olawale afuye <walosha@yahoo.com>
2022-09-15 19:06:54 +0100

    additinal feature

--- templates/application/react/ts/src/App.tsx
+++ templates/application/react/ts/src/App.tsx
@@ -1,14 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// material core
+import { ThemeProvider } from '@mui/material/styles'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import { LocalizationProvider as LocalAdapter } from '@mui/x-date-pickers/LocalizationProvider'
+import themes from './theme'
+import RenderRoutes from './routes'
+import { NotificationProvider } from './context/NotificationProvider'
+import { CssBaseline } from '@mui/material'
 
-import "./index.{{CSS_EXTENSION}}";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0,
+      cacheTime: 600000,
+    },
+    mutations: {
+      useErrorBoundary: false,
+    },
+  },
+})
 
-const App = () => (
-  <div className="{{CONTAINER}}">
-    <div>Name: {{ NAME }}</div>
-    <div>Framework: {{ FRAMEWORK }}</div>
-    <div>Language: {{ LANGUAGE }}</div>
-    <div>CSS: {{ CSS }}</div>
-  </div>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
+function App() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalAdapter dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={themes(0)}>
+          <CssBaseline>
+            <NotificationProvider>
+              <QueryClientProvider client={queryClient}>
+                <RenderRoutes />
+              </QueryClientProvider>
+            </NotificationProvider>
+          </CssBaseline>
+        </ThemeProvider>
+      </LocalAdapter>
+    </LocalizationProvider>
+  )
+}
+
+export default App
